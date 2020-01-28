3af94477c2ad068b8b4156b51391d4093144f974
sio1 <sio1>
2020-01-28 10:31:52 +0100

    commit -m -all

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+
+function alerte(){
+  alert('Veuillez entrer vos coordonnées de carte bleue !');
+}
+
+//Pour changer une image :
+//<button onclick="document.getElementById('image').src='Images/Accueil.jpg'">Turn on the light</button>
+//<img id="image" src="Images/Accueil.jpg" style="width:1000px">
+//<button onclick="document.getElementById('image').src='Images/maison1.jpg'">Turn off the light</button>
